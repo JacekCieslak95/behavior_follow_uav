3105bbb736048df6d77f72f0fe1202f6ecbddf29
JacekCieslak95 <jacek.cieslak95@gmail.com>
2018-01-02 17:23:32 +0100

    Usunięto misje

--- missions/misja_drugi.py
+++
@@ -1,9 +0,0 @@
-import executive_engine_api as api
-#import numpy as np
-#import time
-
-def runMission():
-  
-  api.activateBehavior('SELF_LOCALIZE_BY_ODOMETRY')
-  api.activateBehavior('FOLLOW_UAV', droneID=1)
-
